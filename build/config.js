bd922b3c7cdfb0203c86b62eb61c3ba294865674
Emre Vatansever <vatanseveremre90@gmail.com>
2021-04-21 17:11:15 +0300

    updated

--- build/config.js
+++ build/config.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_URL = exports.DIGEST = exports.LENGTH = exports.SECRET = exports.PORT = void 0;
+exports.DB_URL = exports.SECRET = exports.PORT = void 0;
 exports.PORT = 5001;
 exports.SECRET = "express-api-tutorial";
-exports.LENGTH = 128;
-exports.DIGEST = 'sha256';
 exports.DB_URL = 'mongodb://localhost:27017/mydb';
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
 export const PORT = 5001;
 export const SECRET = "express-api-tutorial";
-export const LENGTH = 128;
-export const DIGEST = 'sha256';
 export const DB_URL = 'mongodb://localhost:27017/mydb';
